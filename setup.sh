2c2c12fc61f4fa96b60d5f3d7b030cf89a5b1959
samuel <samuel.freeman@caterpillar.git.local>
2019-12-12 09:12:26 +1030

    Inital Commit

---
+++ setup.sh
@@ -0,0 +1,94 @@
+#!/bin/bash
+
+
+#function to install apps with a clean display
+function retryinstall
+{
+echo -e "[\033[33m-\e[0m] Retrying..."
+DEBIAN_FRONTEND=noninteractive apt-get --fix-broken install -yq -o  Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" >/dev/null 2>/dev/nul
+DEBIAN_FRONTEND=noninteractive apt-get install --fix-missing -yq -o  Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" >/dev/null 2>/dev/nul
+DEBIAN_FRONTEND=noninteractive apt-get install -yq -o  Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" $1 >/dev/null 2>/dev/null && echo -e "[\033[32m*\e[0m]OK" || echo -e "[\033[31m-\e[0m] FAILED"
+}
+function install
+{
+echo -n "installing:$1 "
+DEBIAN_FRONTEND=noninteractive apt-get install -yq -o  Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" $1 >/dev/null 2>/dev/null && echo -e "[\033[32m*\e[0m]OK" || retryinstall $1
+}
+
+
+
+##### Main #####
+USERN=drop
+
+#Check Sudo
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run with sudo" 
+   exit 1
+fi
+
+#Check working directory
+FILE=.zshrc
+if test -f "$FILE"; then
+    echo "working directory validated"
+    else
+        "Please change to the downloaded direectory with file and run directly from there"
+        exit
+fi
+
+
+
+
+#Prompt for standard username
+while [[ "${answer}" != "y" ]]
+    do
+        echo "What is the instructor users username?"
+        read inuser
+        echo "You have entered ${inuser} as the username for your standard user, is this correct? [y/n]"
+        read answer
+        while [[ ! "${answer}" =~ ^y$|^n$ ]]
+        do
+            echo/ "please select y or n"
+            read answer
+        done 
+ done
+
+
+#Install Instructor Applications
+install vim
+install powerline
+install rssh
+install git
+install zsh
+install curl
+
+#install oh my zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+ensure shell changed
+sudo -u ${inuser} chsh -s /usr/bin/zsh
+
+#Create the drop user
+useradd -m -d /home/${inuser} -s /usr/bin/rssh ${inuser}
+echo "user ${inuser} added successfully!"
+echo ${inuser}:"Drop1" | chpasswd
+echo "Password for user ${inuser} changed successfully to Drop1"
+
+git clone https://github.com/samfree91/linuxtraining.git 
+cp -r linuxtraining /home/$inuser/
+
+#remove my username with set username
+sed -i -e 's/setupuser/$inuser/g' .zshrc
+
+#Enable ssh
+systemctl enable ssh
+systemctl start ssh
+
+#copy files to correct directories
+cp rssh.conf /etc/rssh.conf
+cp .vimrc /home/${inuser}/
+cp .zshrc /home/${inuser}/
+
+
+
+echo "Setup Complete, Please log out and back in"
+echo "Note: Your terminal is now zsh, & the scripts required for course are stored in ~/linuxtraining"
+echo "SCP Username:${inuser}"
