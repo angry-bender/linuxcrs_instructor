775b3f89a80df50d44036801268f633fb40d7645
samfree91 <samfree91@hotmail.com>
2019-12-12 15:24:20 +1030

    Update setup.sh
    Tidying Output

--- setup.sh
+++ setup.sh
@@ -29,55 +29,57 @@ fi
 #Check working directory
 FILE=.zshrc
 if test -f "$FILE"; then
-    echo "working directory validated"
+    echo -e "Working Directory Check [\033[32m*\e[0m]OK"
     else
-        "Please change to the downloaded direectory with file and run directly from there"
+        echo -e "[\033[31m-\e[0m] Working Directory Check FAILED"
+        echo "Please change to the downloaded direectory with file and run directly from there"
+        echo "This script will now exit"
         exit
 fi
 
 #Get the Standard Users username
 inuser=$SUDO_USER
 
+#install oh my zsh
+install zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh 0<&-)" 2>/dev/null 
+echo -e "Oh my ZSH installation [\033[33m-\e[0m] Check after logon"
+ensure shell changed
+usermod -s /usr/bin/zsh ${inuser}
+echo -e "${inuser} shell changed to zsh [\033[32m*\e[0m]OK"
 
 #Install Instructor Applications
 install vim
-install powerline
-install rssh
 install git
-install zsh
 install curl
 install cowsay
 install fortune
-
-#install oh my zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh 0<&-)" 0<&-
-ensure shell changed
-usermod -s /usr/bin/zsh ${inuser}
-
+install powerline
+install rssh
 
 #Create the drop user
 useradd -m -d /home/drop -s /usr/bin/rssh drop
-echo "user drop added successfully!"
+echo -e "Useradd: Drop [\033[32m*\e[0m]OK"
 echo drop:"Drop1" | chpasswd
-echo "Password for user drop changed successfully to Drop1"
+echo -e "Password Set to Drop1 for user: drop [\033[32m*\e[0m]OK"
 
-git clone https://github.com/samfree91/linuxtraining.git 
+git clone https://github.com/samfree91/linuxtraining.git 1&2>/dev/null && echo -e "Training Scripts Clone: [\033[32m*\e[0m]OK" || echo -e "Training Scripts Clone: [\033[31m-\e[0m]  FAILED"
 cp -r linuxtraining /home/$inuser/
 
 #remove my username with set username
 sed -i -e "s/setupuser/"${inuser}"/g" .zshrc
 
 #Enable ssh
-systemctl enable ssh
-systemctl start ssh
+systemctl enable ssh 1&2>/dev/null && echo -e "SSH Service: [\033[32m*\e[0m]OK" || echo -e "SSH Service: [\033[31m-\e[0m]  FAILED"
+systemctl start ssh 1&2>/dev/null && echo -e "Start SSH: [\033[32m*\e[0m]OK" || echo -e "Start SSH: [\033[31m-\e[0m]  FAILED"
 
 #copy files to correct directories
 cp rssh.conf /etc/rssh.conf
 cp .vimrc /home/${inuser}/
 cp .zshrc /home/${inuser}/
+echo -e "Copy Config Files: [\033[32m*\e[0m]OK"
 
 
-
-echo "Setup Complete, Please log out and back in"
+echo -e "[\033[32m*\e[0m]Setup Complete, Please log out and back in"
 echo "Note: Your terminal is now zsh, & the scripts required for course are stored in ~/linuxtraining"
 echo "SCP Username:drop, Password Drop1"
